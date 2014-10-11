2c4135f1ecbe23cdb0cddcee6432bc827e8de059
AaronBoat <abaoteng7@gmail.com>
2014-10-11 15:20:08 +0000

    Start muvie review app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moviereview::Application.config.secret_key_base = '376d8c7aa871c3986f12f4fa4532865dae76fc3d39d4a798fcf2093b45de48aa10323ee364733a76d8a51059dc6d984f3d7038346e2a1bd7a08344ad6fc06fa5'
