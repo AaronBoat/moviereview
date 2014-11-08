3cd8cdd61028d3c2d3547aa1db4d71e21d56d8c1
AaronBoat <aboateng7@gmail.com>
2014-11-08 11:18:13 +0000

    Added images to app

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -50,6 +50,6 @@ class MoviesController < ApplicationController
     end
 
     def movie_params
-      params.require(:movie).permit(:title, :release_year, :price, :description, :director, :stock)
+      params.require(:movie).permit(:title, :release_year, :price, :description, :director, :stock, :poster)
     end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,5 +5,8 @@ class Movie < ActiveRecord::Base
 	validates :title, uniqueness: true
 	validates :stock, numericality: { only_integer: true}
 	validates :price, numericality: { greater_than_or_equal_to: 0.01}
+
+	has_attached_file :poster, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpg"
+  	validates_attachment_content_type :poster, :content_type => /\Aimage\/.*\Z/
 end
  
